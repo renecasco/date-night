95e264c1c685523fba9db0f71c61feee27fcac37
Rene Casco <34440390+renecasco@users.noreply.github.com>
2019-05-10 10:57:27 -0600

    completes node class, all tests passing

--- lib/node.rb
+++ lib/node.rb
@@ -1,3 +1,13 @@
 class Node
+  attr_reader :score
+  attr_reader :movie
+  attr_accessor :left
+  attr_accessor :right
 
+  def initialize(score, movie, left = nil, right = nil)
+    @score = score
+    @movie = movie
+    @left = left
+    @right = right
+  end
 end
