2cba12d8be10fe24c4c6da31b3af0880e86f59ee
Rene Casco <34440390+renecasco@users.noreply.github.com>
2019-05-10 11:06:34 -0600

    corrects binary_seacrh_tree_test and creates binary_search_tree class

--- test/binary_search_tree_test.rb
+++ test/binary_search_tree_test.rb
@@ -1,7 +1,7 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require 'pry'
-require './lib/node'
+require './lib/binary_search_tree'
 require 'simplecov'
 
 class BinarySearchTreeTest < Minitest::Test
